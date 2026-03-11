73b9359a506b6114282bbedb08093016e6c07b26
Beatriz Samaha <biagarcia2806@gmail.com>
2026-03-11 00:22:07 -0300

    feat(app): cria app.js com configuração do Express e middlewares base

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// middleware de log, middleware de erro global e app.listen(3000). NÃO registrar rotas ainda.
+// require do express 
+const express = require('express'); // Importa o módulo
+const app = express(); // Cria a aplicação (app)
+
+// expreess.json
+app.use(express.json());
+
+//middleware de log,
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] Nova requisição recebida: ${req.method} ${req.path}`);
+  next();
+});
+
+//middleware de erro global
+app.use((err, req, res, next) => {
+  res.status(500).json({ erro: err.message });
+});
+
+//app.listen(3000)
+app.listen(3000, () => console.log('Servidor iniciado na porta 3000'));
