9312815c49678920aef386cec4afee4c82b03136
Beatriz Samaha <biagarcia2806@gmail.com>
2026-03-11 00:32:03 -0300

    feat(produtos): cria estrutura inicial do controller com funções vazias

---
+++ src/controllers/produtosController.js
@@ -0,0 +1,31 @@
+//lógica de cada operação CRUD
+
+let produtos = [];
+let nextId = 1;
+
+// GET /produtos - listar todos
+const listar = (req, res) => {
+  // TODO
+};
+
+// GET /produtos/:id - buscar por ID
+const buscarPorId = (req, res) => {
+  // TODO
+};
+
+// POST /produtos - criar
+const criar = (req, res) => {
+  // TODO
+};
+
+// PUT /produtos/:id - atualizar
+const atualizar = (req, res) => {
+  // TODO
+};
+
+// DELETE /produtos/:id - remover
+const remover = (req, res) => {
+  // TODO
+};
+
+module.exports = { listar, buscarPorId, criar, atualizar, remover };
